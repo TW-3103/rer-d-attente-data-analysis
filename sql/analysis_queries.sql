c391f4179a790842f96f83c74beb345c25c71140
TW-3103 <Wangmymy@hotmail.com>
2026-02-28 20:23:23 +0100

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,1 +1,24 @@
+-- ===============================
+-- 25 responses
+-- Analysis 1: Peak hour vs Time Pressure
+-- ===============================
 
+WITH heure_de_pointe_cte AS (
+    SELECT
+        Quelles_sont_les_tranches_horaires_dans_lesquelles_vous_prenez_le_RER_D AS horaire,
+        CASE
+            WHEN Quelles_sont_les_tranches_horaires_dans_lesquelles_vous_prenez_le_RER_D LIKE '%heures de pointes%'
+                THEN 'Pointe'
+            ELSE 'Hors pointe'
+        END AS heure_de_pointe,
+        Vous_sentez_vous_press_par_le_temps_lorsque_vous_devez_prendre_les_transports AS pression
+    FROM `projet-prememoire-rerd.prememoire_rerd.Reponses_RERD_GSHEET`
+)
+
+SELECT
+    heure_de_pointe,
+    pression,
+    COUNT(*) AS nb_reponses
+FROM heure_de_pointe_cte
+GROUP BY heure_de_pointe, pression
+ORDER BY heure_de_pointe, pression;
