5ddf244e030d2ebe5a8f9f964ae39deedae6c940
TW-3103 <Wangmymy@hotmail.com>
2026-03-08 18:33:37 +0100

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -9,13 +9,13 @@
 WITH base AS (
     SELECT
         CASE
-            WHEN Quelles_sont_les_tranches_horaires_dans_lesquelles_vous_prenez_le_RER_D LIKE '%heures de pointes%'
+            WHEN Quelles_sont_les_tranches_horaires_dans_lesquelles_vous_prenez_le_RER_D___ LIKE '%heures de pointes%'
                 THEN 'Pointe'
             ELSE 'Hors pointe'
         END AS heure_de_pointe,
 
         CASE
-            WHEN Vous_sentez_vous_press_par_le_temps_lorsque_vous_devez_prendre_les_transports IN ('Oui', 'Parfois')
+            WHEN Vous_sentez_vous_press___par_le_temps_lorsque_vous_devez_prendre_les_transports__ IN ('Oui', 'Parfois')
                 THEN 'Pression'
             ELSE 'Pas pression'
         END AS pression_binaire
